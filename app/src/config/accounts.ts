e548339390b40d3b567fdef6236f29ceb8091976
gabrielhicks <gabrielhicks@mail.com>
2022-08-13 23:20:53 -0400

    Remoev personal wallet

--- app/src/config/accounts.ts
+++ app/src/config/accounts.ts
@@ -3,8 +3,8 @@ import { TESTING } from './misc';
 import { DISPENSER_PROGRAM_ID, DISPENSER_REGISTRY_ADDRESS } from './programIds';
 
 export const ADMIN_ACCOUNTS = TESTING
-  ? ['2fquRXDqK1YLLC8E7z74CtLevw1ZiTtd6LG3L2bQsZdM']
-  : ['2fquRXDqK1YLLC8E7z74CtLevw1ZiTtd6LG3L2bQsZdM'];
+  ? ['2zcTAzzHmvVPWjpFL5PnHhF6yutr5VXq61xueshhPwnU']
+  : ['86f5xSDrue8Zz4QfuMErURN3o393L72zA1J2WRsYeTGB'];
 
 export const [VAULT_TOKEN_IN] = utils.publicKey.findProgramAddressSync(
   [Buffer.from('vault_token_in'), DISPENSER_REGISTRY_ADDRESS.toBytes()],
